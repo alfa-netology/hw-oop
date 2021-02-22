25c109561200ec981c4bb44f73d610b45299dd27
Alexsander Uglin <uglin76@mail.ru>
2021-02-22 20:42:02 +0300

    feat: Override method __str__ to Student, Lecture(Mentor), Reviewer(Mentor)

--- modules/own_classes.py
+++ modules/own_classes.py
@@ -6,6 +6,12 @@ class Student:
         self.finished_courses = ['Введение в программирование']
         self.courses_in_progress = []
 
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}\n" \
+               f"Средняя оценка за домашние задания :  {self.grades['average']}\n" \
+               f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n" \
+               f"Завершенные курсы: {', '.join(self.finished_courses)}"
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -16,6 +22,14 @@ class Lecture(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
+    def __str__(self):
+        return f"Имя: {self.name}\n" \
+               f"Фамилия: {self.surname}\n" \
+               f"Средняя оценка за лекции: {self.grades['average']}"
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"

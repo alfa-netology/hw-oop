2250655e3c2e325ea9a376a1233e44fa62acc218
Alexsander Uglin <uglin76@mail.ru>
2021-02-22 21:02:42 +0300

    feat: Add the ability grading Student->Lector & Rewiewer->Student

--- modules/own_classes.py
+++ modules/own_classes.py
@@ -1,3 +1,5 @@
+import modules.own_functions as functions
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -5,6 +7,14 @@ class Student:
         self.gender = gender
         self.finished_courses = ['Введение в программирование']
         self.courses_in_progress = []
+        """
+        grades:
+            'average' - срендяя оценка
+            'counter' - количество оценок
+            'sum' - сумма всех проставленных оценко
+            'history' : { 'course_name' : [grades] }
+        """
+        self.grades = {'average': 0, 'counter': 0, 'sum': 0, 'history': {}}
 
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}\n" \
@@ -12,20 +22,35 @@ class Student:
                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n" \
                f"Завершенные курсы: {', '.join(self.finished_courses)}"
 
+    def rate_lecture(self, lecture=None, course=None, grade=None):
+        """ выставляет оценку лектору """
+        functions.dispatcher(self, lecture, Lecture, course, grade)
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+
 class Lecture(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        """
+        grades:
+            'average' - срендяя оценка
+            'counter' - количество оценок
+            'sum' - сумма всех проставленных оценко
+            'history' : { 'course_name' : [grades] }
+        """
+        self.grades = {'average': 0, 'counter': 0, 'sum': 0, 'history': {}}
 
     def __str__(self):
         return f"Имя: {self.name}\n" \
                f"Фамилия: {self.surname}\n" \
                f"Средняя оценка за лекции: {self.grades['average']}"
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -33,3 +58,6 @@ class Reviewer(Mentor):
 
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}"
+
+    def rate_hw(self, student=None, course=None, grade=None):
+        functions.dispatcher(self, student, Student, course, grade)
---
+++ modules/own_functions.py
@@ -0,0 +1,94 @@
+import inspect
+"""
+набор вспомогательных функций:
+    check_args(*args), check_grades(grade), check_instance(obj, parent_class),
+    check_student_course(obj, course), check_lecture_course(obj, course) - отвечают за проверку входящих данных
+    
+    set_grade(obj, course, grade) - проставляет оценки
+    
+    calc_grade_average(obj, grade) - считает среднюю оценку для obj
+    
+    dispatcher(self, obj, parent_class, course, grade) регламентирует действие всех вышеперечисленных
+    функций в зависимости из какого класса его вызвали  
+
+"""
+
+def dispatcher(self, obj, parent_class, course, grade):
+    if check_args(obj, course, grade) is True:
+        if check_grades(grade) is True:
+            if check_instance(obj, parent_class) is True:
+                if parent_class.__name__ == 'Student' and check_student_course(obj, course) is True:
+                    set_grade(obj, course, grade)
+                    calc_grade_average(obj, grade)
+                elif parent_class.__name__ == 'Lecture' and check_student_course(self, course) is True:
+                    if check_lecture_course(obj, course):
+                        set_grade(obj, course, grade)
+                        calc_grade_average(obj, grade)
+
+def check_args(*args):
+    """ проверяет переданы ли все аргументы при вызове функции """
+    # скорее всего это лишнее, но было интересно реализовать
+    if None not in args:
+        return True
+    else:
+        module_name = inspect.stack()[2].filename
+        line = inspect.stack()[2].lineno
+        function_name = inspect.stack()[1].function
+        print(f'ArgumentsError: {module_name}, строка {line}, {function_name}() передано недостаточно аргументов')
+        # не уверен в необходимости возвращать False, поправтье, если не прав
+        # return False
+
+def check_grades(grade):
+    if grade in range(1, 11):
+        return True
+    else:
+        module_name = inspect.stack()[2].filename
+        line = inspect.stack()[2].lineno
+        function_name = inspect.stack()[1].function
+        print(
+            f'ValueError: {module_name}, строка {line}, {function_name}() {grade} не целое число в диапозоне от 1 до 10')
+        # return False
+
+def check_instance(obj, parent_class):
+    if isinstance(obj, parent_class):
+        return True
+    else:
+        print(f'ValueError: {obj} не является потомком {parent_class.__name__}()')
+        # return False
+
+def check_student_course(obj, course):
+    """ проверяет обучается student на курсе или нет"""
+    if course in obj.courses_in_progress:
+        return True
+    else:
+        print(f'ValueError: {obj.name} {obj.surname} не является студентом курса {course}')
+        # return False
+
+def check_lecture_course(obj, course):
+    """ провераяет закреплен lecture за курсом или нет"""
+    if course in obj.courses_attached:
+        return True
+    else:
+        print(f'ValueError: {obj.name} {obj.surname} не преподаёт {course}')
+        # return False
+
+def set_grade(obj, course, grade):
+    """
+    выставляет оценку
+        args:
+            obj(object): экземпляр класса для работы\n
+            course(str): название курса\n
+            grade(int): оценка\n
+    """
+    if course in obj.grades['history']:
+        obj.grades['history'][course] += [grade]
+    else:
+        obj.grades['history'][course] = [grade]
+
+
+def calc_grade_average(obj, grade):
+    """ считает среднюю оценку """
+    obj.grades['counter'] += 1
+    obj.grades['sum'] += grade
+    average = round(obj.grades['sum'] / obj.grades['counter'], 1)
+    obj.grades['average'] = average
